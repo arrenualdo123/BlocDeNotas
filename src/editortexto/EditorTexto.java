8f5f05179a1bbf0b668040058fa9e179ea63f4ae
arrenualdo123 <jesearreola13@gmail.com>
2024-03-01 21:01:24 -0600

    first version

---
+++ src/editortexto/EditorTexto.java
@@ -0,0 +1,17 @@
+
+package editortexto;
+
+/**
+ *
+ * @author 2B
+ */
+public class EditorTexto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Editor().setVisible(true);
+    }
+    
+}
